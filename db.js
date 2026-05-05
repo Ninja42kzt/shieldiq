be0c110beef2b90126cd879277f7bb66445018e3
Ninja42kzt <nicholasmatata2@gmail.com>
2026-05-05 23:08:46 +0300

    Persistent database path for production

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'shieldiq.db'));
+const dbPath = process.env.NODE_ENV === 'production'
+    ? '/data/shieldiq.db'
+    : path.join(__dirname, 'shieldiq.db');
+
+const db = new Database(dbPath);
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS users (
@@ -32,5 +36,5 @@ db.exec(`
     );
 `);
 
-console.log('Database initialized');
+console.log('Database initialized at:', dbPath);
 module.exports = db;
